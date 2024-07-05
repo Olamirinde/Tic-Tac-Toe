30a26dd70188a25dbce01ae73270014db656b29e
Your Name <rindepixels@gmail.com>
2024-07-05 16:39:05 +0100

    Added click functionality

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,6 +1,9 @@
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button className="float-left mr-[-1px] mt-[-1px] h-[100px] w-[100px] cursor-pointer border border-red-700 p-0 text-center text-[40px]">
+    <button
+      onClick={onClick}
+      className="float-left mr-[-1px] mt-[-1px] h-[100px] w-[100px] cursor-pointer border border-red-700 p-0 text-center text-[40px]"
+    >
       {value}
     </button>
   );
--- tic-tac-toe/src/components/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import Square from "./Square";
 
 const TicTacToe = () => {
+  const [square, setSquare] = useState(Array(9).fill(""));
+  const [isXturn, setIsXTurn] = useState(true);
+
+  function handleClick(getCurrentSquare) {
+    let newSquares = [...square];
+    newSquares[getCurrentSquare] = isXturn ? "X" : "O";
+    setIsXTurn(!isXturn);
+    setSquare(newSquares);
+  }
+
   return (
-    <div className="clear-both table content-none">
+    <div className="clear-both table bg-gray-200 content-none">
       <div className="row1">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onClick={() => handleClick(0)} />
+        <Square value={square[1]} onClick={() => handleClick(1)} />
+        <Square value={square[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row2">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} onClick={() => handleClick(3)} />
+        <Square value={square[4]} onClick={() => handleClick(4)} />
+        <Square value={square[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row3">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} onClick={() => handleClick(6)} />
+        <Square value={square[7]} onClick={() => handleClick(7)} />
+        <Square value={square[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
